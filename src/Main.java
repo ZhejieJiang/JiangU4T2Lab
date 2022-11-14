d6a812fea1ec5fb6650e9d4d87d6f5c9f4d2b3e7
Zhejie Jiang <zhejiej@nycstudents.net>
2022-11-14 09:28:40 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[]args) {
+
+        for (int x = 1; x < 11; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 0; x < 10; x++) {
+            System.out.print(x + 1 + " ");
+        }
+        System.out.println();
+
+        for (int x = 15; x >= 0; x--) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 16; x > 0; x--) {
+            System.out.print(x - 1 + " ");
+        }
+        System.out.println();
+
+        for (int x = 1; x <= 15; x++) {
+            if (x % 2 != 0) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+
+        for (int x = 1; x <= 15; x += 2) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+
+        for (int x = 4; x <= 11; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 3; x < 11; x++) {
+            System.out.print(x + 1 + " ");
+        }
+        System.out.println();
+
+
+        for (int x = 4; x <= 14; x++) {
+            if (x % 2 == 0) {
+                System.out.print(x + " ");
+            }
+        }
+
+        for (int x = 4; x <= 14; x += 2) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 0; x <= 100; x++) {
+            if (x % 6 == 0) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+
+        for (int x = 0; x <= 100; x+=6){
+                System.out.print(x + " ");
+            }
+        }
+
+    }
+
+
+
+
+
+
+
